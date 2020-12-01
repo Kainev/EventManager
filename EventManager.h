6d7c53c867732890e4c2465e344253d5b720cebd
Kaine van Gemert <kainevangemert@gmail.com>
2020-12-01 13:57:31 +1100

    Replaced tabs with spaces

--- EventManager.h
+++ EventManager.h
@@ -10,101 +10,100 @@ typedef std::size_t EventID;
 
 
 struct CallbackContainerBase {};
-	
+    
 template<typename T>
 struct CallbackContainer : CallbackContainerBase
 {
-	CallbackContainer()
-	{
-		event = new T();
-	}
+    CallbackContainer()
+    {
+        event = new T();
+    }
 
-	~CallbackContainer()
-	{
-		delete event;
-	}
+    ~CallbackContainer()
+    {
+        delete event;
+    }
 
-	std::vector<std::function<void(T*)>> callbacks;
-	T* event;
+    std::vector<std::function<void(T*)>> callbacks;
+    T* event;
 };
 
 
 class EventManager
 {
 public:
-	template<typename T, typename T_Function>
-	static void listen(T_Function callback);
+    template<typename T, typename T_Function>
+    static void listen(T_Function callback);
 
-	template<typename T, typename T_Instance, typename T_Function>
-	static void listen(T_Instance& instance, T_Function callback);
+    template<typename T, typename T_Instance, typename T_Function>
+    static void listen(T_Instance& instance, T_Function callback);
 
-	template<typename T, typename T_Instance, typename T_Function>
-	static void listen(T_Instance* instance, T_Function callback);
+    template<typename T, typename T_Instance, typename T_Function>
+    static void listen(T_Instance* instance, T_Function callback);
 
-	template<typename T, typename... T_Args>
-	static void fire(T_Args...args);
-
-private:
-	template<typename T>
-	static auto get_event_id()->EventID;
-
-	template<typename T>
-	static auto register_event()->EventID;
-
+    template<typename T, typename... T_Args>
+    static void fire(T_Args...args);
 
 
 private:
-	static inline std::vector<CallbackContainerBase*>	s_callbacks;
-	static inline std::vector<void*>					s_events;
+    template<typename T>
+    static auto get_event_id()->EventID;
 
-	static inline EventID s_next_event_id = 0u;
+    template<typename T>
+    static auto register_event()->EventID;
+
+private:
+    static inline std::vector<CallbackContainerBase*>	s_callbacks;
+    static inline std::vector<void*>					s_events;
+
+    static inline EventID s_next_event_id = 0u;
 };
 
 
 template<typename T, typename T_Function>
 inline void EventManager::listen(T_Function callback)
 {
-	static_cast<CallbackContainer<T>*>(s_callbacks[get_event_id<T>()])->callbacks.emplace_back(callback);
+    static_cast<CallbackContainer<T>*>(s_callbacks[get_event_id<T>()])->callbacks.emplace_back(callback);
 }
 
 
 template<typename T, typename T_Instance, typename T_Function>
 inline void EventManager::listen(T_Instance& instance, T_Function callback)
 {
-	static_cast<CallbackContainer<T>*>(s_callbacks[get_event_id<T>()])->callbacks.emplace_back([&instance, callback](T* event) { (instance.*callback)(event); });
+    static_cast<CallbackContainer<T>*>(s_callbacks[get_event_id<T>()])->callbacks.emplace_back([&instance, callback](T* event) { (instance.*callback)(event); });
 }
 
 template<typename T, typename T_Instance, typename T_Function>
 inline void EventManager::listen(T_Instance* instance, T_Function callback)
 {
-	static_cast<CallbackContainer<T>*>(s_callbacks[get_event_id<T>()])->callbacks.emplace_back([instance, callback](T* event) { (instance->*callback)(event); });
+    static_cast<CallbackContainer<T>*>(s_callbacks[get_event_id<T>()])->callbacks.emplace_back([instance, callback](T* event) { (instance->*callback)(event); });
 }
 
 
 template<typename T, typename ...T_Args>
 inline void EventManager::fire(T_Args ...args)
 {
-	EventID id = get_event_id<T>();
+    EventID id = get_event_id<T>();
 
-	CallbackContainer<T>* callback_container = reinterpret_cast<CallbackContainer<T>*>(s_callbacks[id]);
-	*callback_container->event = T{ args... };
-	for (auto& callback : callback_container->callbacks)
-		callback(callback_container->event);
+    CallbackContainer<T>* callback_container = reinterpret_cast<CallbackContainer<T>*>(s_callbacks[id]);
+    *callback_container->event = T{ args... };
+    for (auto& callback : callback_container->callbacks)
+        callback(callback_container->event);
 }
 
 
 template<typename T>
 inline EventID EventManager::get_event_id()
 {
-	static EventID event_id = register_event<T>();
-	return event_id;
+    static EventID event_id = register_event<T>();
+    return event_id;
 }
 
 
 template<typename T>
 inline EventID EventManager::register_event()
 {
-	s_callbacks.emplace_back(new CallbackContainer<T*>());
-	
-	return s_next_event_id++;
+    s_callbacks.emplace_back(new CallbackContainer<T*>());
+    
+    return s_next_event_id++;
 }
